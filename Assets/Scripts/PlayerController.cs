cb5f90fab648521904e876c1a38d8253ccfbd7f0
DominicAudibert <da01665@srcs.org>
2021-12-02 15:39:36 -0800

    Tutorial 7 Complete

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
+
 
 
 
@@ -9,13 +11,28 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 10;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
 
     private Rigidbody rb;
+    private int count;
     private float movementX;
     private float movementY;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winTextObject.SetActive(false);
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count" + count.ToString();
+        if(count >= 9)
+        {
+            winTextObject.SetActive(true);
+        }
     }
 
     void OnMove(InputValue movementValue)
@@ -35,13 +52,9 @@ public class PlayerController : MonoBehaviour
     {
         if (other.gameObject.CompareTag("PickUp"))
         {
-            other.gameObject.SetActive(false); 
+            other.gameObject.SetActive(false);
+            count = count + 1;
         }
-        
+
     }
-
-
-
-
-
 }
